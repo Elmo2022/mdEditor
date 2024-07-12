5e12e04edc085fb81fdbfe94e25f1c062cfb9d68
Elmo2022 <2191601893@qq.com>
2024-07-12 23:43:32 +0800

    基本功能

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import VMdEditor from '@kangc/v-md-editor';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import createKatexPlugin from '@kangc/v-md-editor/lib/plugins/katex/cdn';
+import katex from 'katex';
+
+VMdEditor.use(githubTheme);
+VMdEditor.use(createKatexPlugin());
+
+const data:string = ref("### 1111"); 
+
+
+function downloadMarkdown() {
+//   const markdownContent = `# 这是一个 Markdown 标题
+
+// 这是一些 Markdown 内容
+
+// - 列表项 1
+// - 列表项 2
+// - 列表项 3`;
+const markdownContent = data.value;
+  const blob = new Blob([markdownContent], { type: 'text/markdown;charset=utf-8' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = 'hello.md';
+  link.click();
+}
+</script>
+
+<template>
+      你好啊
+
+      <v-md-editor  v-model="data" />
+      
+
+      这是下面
+
+
+<button @click="downloadMarkdown">下载</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+createApp(App).use(VMdEditor).mount('#app')
+import VuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+import markdownItKatex from 'markdown-it-katex';
+import 'katex/dist/katex.min.css';
+
+VMdEditor.use(VuepressTheme);
+
+// 配置 markdown-it 插件
+// VMdEditor.extendMarkdown((md:any) => {
+//   md.use(markdownItKatex);
+// });
